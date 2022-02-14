23305ce3095f3dcd6753b0e7befa379febf7236e
drandreaskrueger <drandreaskrueger@users.noreply.github.com>
2022-02-14 15:43:00 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="drandreaskrueger"
+original_name="btceurhist"
+original_urlname="btceurhist"
+original_description="Awesome btceurhist created by drandreaskrueger"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv btceurhist $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
