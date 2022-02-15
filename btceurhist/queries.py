302336e1490cfbc0ccde7f1414b19f241930f669
Andreas Krueger <coding@AndreasKrueger.de>
2022-02-15 18:38:31 +0100

    flake8 linting issues: (almost) all corrected

--- btceurhist/queries.py
+++ btceurhist/queries.py
@@ -40,83 +40,89 @@ def caller(url):
 
 
 def jsoner(j, jpath):
-    if type(j)==type("a string"):
-        return j # then the requests.get had resulted in an error
-    
-    answer=j
+    if isinstance(j, str):
+        return j  # then the requests.get had resulted in an error
+
+    answer = j
     try:
         for dive in jpath:
-            answer=answer[dive]
+            answer = answer[dive]
     except Exception as e:
-        answer="(%s) %s for %s" % (type(e), e, j)        
-        
+        answer = "(%s) %s for %s" % (type(e), e, j)
+
     return answer
 
+
 def btcusd(date, urltemplate=COINDESK_BTCUSD):
-    # params={"startdate" :"2020-01-01","enddate" :"2020-01-05"}  
-    params={"startdate":date,"enddate":date}
-    url=urltemplate.format(**params)
-    j=caller(url)
-    price=jsoner(j, ['bpi', date])
+    # params={"startdate" :"2020-01-01","enddate" :"2020-01-05"}
+    params = {"startdate": date, "enddate": date}
+    url = urltemplate.format(**params)
+    j = caller(url)
+    price = jsoner(j, ['bpi', date])
     return price
+
 
 def usdeur_foorilla(date, urltemplate=FOORILLA_USDEUR):
-    # params={"startdate" :"2020-01-01","enddate" :"2020-01-05"}  
-    params={"startdate":date,"enddate":date}
-    url=urltemplate.format(**params)
-    j=caller(url)
+    # params={"startdate" :"2020-01-01","enddate" :"2020-01-05"}
+    params = {"startdate": date, "enddate": date}
+    url = urltemplate.format(**params)
+    j = caller(url)
     # print (j)
-    pricedict=jsoner(j, ['results'])
-    if type(pricedict)==type("a string"):
-        return pricedict # error message
-    price=jsoner(pricedict[0], ['value'])
+    pricedict = jsoner(j, ['results'])
+    if isinstance(pricedict, str):
+        return pricedict  # error message
+    price = jsoner(pricedict[0], ['value'])
     return price
+
 
 def usdeur_oxr(date, urltemplate=OXR_USDEUR, app_id=APP_ID):
-    # params={"startdate" :"2020-01-01","enddate" :"2020-01-05"}  
-    params={"date":date,"app_id":app_id, "symbols": "EUR"}
-    url=urltemplate.format(**params)
+    # params={"startdate" :"2020-01-01","enddate" :"2020-01-05"}
+    params = {"date": date, "app_id": app_id, "symbols": "EUR"}
+    url = urltemplate.format(**params)
     # print (url)
-    j=caller(url)
+    j = caller(url)
     # print (j)
-    price=jsoner(j, ['rates', "EUR"])
+    price = jsoner(j, ['rates', "EUR"])
     return price
 
-usdeur=usdeur_oxr
+
+usdeur = usdeur_oxr
+
 
 def btceur(date):
     in_usd = btcusd(date)
     to_eur = usdeur(date)
     try:
         answer = float(in_usd) * float(to_eur)
-    except:
+    except Exception:
         answer = "cannot multiply: (%s, %s)" % (in_usd, to_eur)
     return answer
     return 10000
 
 
 def oxr_usage(urltemplate=OXR_USAGE, app_id=APP_ID):
-    params={"app_id":app_id}
-    url=urltemplate.format(**params)
+    params = {"app_id": app_id}
+    url = urltemplate.format(**params)
     # print (url)
-    j=caller(url)
-    u=jsoner(j,["data", "usage"])
+    j = caller(url)
+    u = jsoner(j, ["data", "usage"])
     answer = pprint.pformat(u)
     # print (answer)
     return answer
-    
-    
+
+
 CALLER = {"btcusd": btcusd,
           "usdeur": usdeur,
           "btceur": btceur
           }
+
 
 def pairprice(pair, date):
     # print (pair, date)
     answer = "%s" % (CALLER[pair](date))
     rowcache.append([date, pair, answer])
     return answer
-    
+
 
 def test_all_queries():
     print(oxr_usage())
@@ -132,11 +138,10 @@ def test_all_queries():
     date="2021-03-77"; print (btceur(date))
     print()
     """
-    print (pairprice("btcusd", "2020-01-01"))
-    print (pairprice("usdeur", "2020-01-01"))
-    print (pairprice("btceur", "2020-01-01"))
-    
+    print(pairprice("btcusd", "2020-01-01"))
+    print(pairprice("usdeur", "2020-01-01"))
+    print(pairprice("btceur", "2020-01-01"))
+
 
 if __name__ == '__main__':
     test_all_queries()
-    
